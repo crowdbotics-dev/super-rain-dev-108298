f32cb2b7df2c30f650c6f60fc2c0d8b7805927c9
Crowdbotics <engineering@crowdbotics.com>
2023-08-10 23:07:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-rain-dev-108298",
+  url: "https://super-rain-dev-108298.botics.co",
+  api: "https://super-rain-dev-108298.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
